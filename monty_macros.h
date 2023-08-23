11a0745da892ba71a60f02c83bb0947eb3763e12
Eniola011 <yettyagbalu@gmail.com>
2023-08-23 09:49:43 -0700

    var

--- monty_macros.h
+++ monty_macros.h
@@ -5,4 +5,18 @@
 #define MAX_TOKENS 1024
 #define DELIM " \n"
 
+/**
+ * @temp: starting point in a doubly linked list
+ * @fileptr: points to file
+ *
+ *
+ */
+typedef struct global_variable
+{
+	stack_t *temp;
+	FILE *fileptr;
+	char *argmt;
+	char *buff;
+} my_globalvar;
+
 #endif
