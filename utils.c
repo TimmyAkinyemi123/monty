93b7a12bbfd2bf24ec8bfc8746a57de82eeea15b
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-08-23 05:02:31 -0700

    Implemented is_opcode function

--- utils.c
+++ utils.c
@@ -9,3 +9,24 @@ void init_data(monty_data *data)
 	data->line_num = 0;
 	data->args = NULL;
 }
+
+/**
+ * is_opcode - checks if string is an opcode
+ * @data: monty program data
+ * Return: function or NOT_OPCODE
+ */
+int is_opcode(monty_data *data)
+{
+	int i = 0;
+	instruction_t instructions[] = {
+		{"push" monty_push},
+		{NULL, NULL}};
+
+	while (instructions[i].opcode)
+	{
+		if (strcmp(data->args[0], instructions[i].opcode))
+			return(instructions[i].f(&stack, data->line_num));
+		i++;
+	}
+	return (NOT_OPCODE);
+}
