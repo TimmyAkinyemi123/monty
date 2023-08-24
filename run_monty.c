edd5ccbd9e01cfd69a4eb7d3c0db6697bd6c2129
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-08-24 13:14:34 -0700

    removed conflicts

--- run_monty.c
+++ run_monty.c
@@ -24,35 +24,16 @@ int monty_code(FILE *fd, monty_data *data)
 			line[strlen(line) - 1] = '\0';
 		if (line[0] == '\0')
 			continue;
-<<<<<<< HEAD
-		data->line = line;
-		tokenize_input(data_ptr);
-=======
 		data->line = _strdup(line);
 		tokenize_input(data);
->>>>>>> a7573008b39e961c729ae09da345ca282564e4d1
-
 		if (is_opcode(&stack, data) == NOT_OPCODE)
 		{
-<<<<<<< HEAD
 			fprintf(stderr, "L%u: unknown instruction %s\n",
 					line_number, data->args[0]);
 			exit_status = EXIT_FAILURE;
-			free_tokens();
-			free(line);
-			free_dlistint(stack);
+			free_tokens(data);
+			free(data->line);
 			break;
-=======
-			if (is_opcode(&stack, data) == NOT_OPCODE)
-			{
-				fprintf(stderr, "L%u: unknown instruction %s\n",
-						line_number, data->args[0]);
-				exit_status = EXIT_FAILURE;
-				free_tokens(data);
-				free(data->line);
-				break;
-			}
->>>>>>> a7573008b39e961c729ae09da345ca282564e4d1
 		}
 		free_tokens(data);
 	}
