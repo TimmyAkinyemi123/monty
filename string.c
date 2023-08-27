c032fc651e3596be570442ae8a65cd45209c4b6b
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2023-08-27 07:33:13 -0700

    handles negative numbers

--- string.c
+++ string.c
@@ -28,16 +28,13 @@ char *_strdup(char *str)
  */
 int is_number(char *str)
 {
-	int i = 0;
-
 	if (!str)
 		return (0);
-	if (str[0] == '-')
-		i++;
-	for (; str[i]; i++)
+	while (*str)
 	{
-		if (!isdigit(str[i]))
+		if (*str < '0' || *str > '9')
 			return (0);
+		str++;
 	}
 	return (1);
 }
